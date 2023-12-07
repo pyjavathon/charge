0ca0f03e61f27eaf4563cdd25d41329248137578
Your ID <your email address>
2023-12-07 23:18:25 +0900

    댓글 삭제 오류(삭제 차례대로  되는 것), 댓글 수정 마무리 작업 중

--- src/main/java/com/evstation/charge/board/controller/CommentController.java
+++ src/main/java/com/evstation/charge/board/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,4 +35,10 @@ public class CommentController {
 	public ResponseEntity<?> delComment(@PathVariable Long commentId) {
 		return ResponseEntity.ok(comSer.delComment(commentId));
 	}
+	
+	/*
+	 * @PutMapping("/{commentId}") public ResponseEntity<?>
+	 * modifyComment(@PathVariable Long boardId, @RequestBody CommentRequestDto
+	 * dto){ return ResponseEntity.ok(comSer.modifyComment(commentId)); }
+	 */
 }
